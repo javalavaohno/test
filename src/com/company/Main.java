c93f6b4885fde4386cf9f4b81cda27ce6a576516
Benjamin Zimmerman <blzimme000@birdvilleschools.net>
2021-09-02 11:41:28 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+        /*
+        // INPUT
+
+        // Movie name
+        System.out.println("Please enter the movie name");
+        String movieTitle = scan.nextLine();
+
+        // Time
+        System.out.println();
+        System.out.println("Please enter the running time in minutes.");
+        int runningTime = scan.nextInt();
+        System.out.println();
+
+        // Movie Site Ratings
+        System.out.println("Please enter ratings from the movie review website.");
+        int siteRating1 = scan.nextInt();
+        int siteRating2 = scan.nextInt();
+        int siteRating3 = scan.nextInt();
+        System.out.println();
+
+        // Focus Group Rating
+        System.out.println("Please enter ratings from the focus group.");
+        double focusGroup1 = scan.nextDouble();
+        double focusGroup2 = scan.nextDouble();
+        System.out.println();
+
+        // Movie Critic Rating
+        System.out.println("Please enter ratings from the focus group.");
+        double movieCritic = scan.nextDouble();
+        System.out.println();
+
+        // MATH
+        int timeHours = runningTime / 60;
+        int timeMinutes = runningTime % 60;
+        double avgSite = (double) (siteRating1+siteRating2+siteRating3) / 3;
+        double avgFocusGroup = (double) (focusGroup1+focusGroup2) / 2;
+        int overallRating = (int) ((avgSite*.2)+(avgFocusGroup*.3)+(movieCritic*.5)+0.5);
+
+        // Info Display
+        System.out.println("Title: " + movieTitle);
+        System.out.println("Running time: " + timeHours + "h" + timeMinutes);
+        System.out.println("Average website rating: " + avgSite);
+        System.out.println("Average focus group rating: " + avgFocusGroup);
+        System.out.println("Average movie critic rating: " + movieCritic);
+        System.out.println("Overall movie rating: " + overallRating);
+        */
+
+        // hello
+                
+
+
+
+
+        }
+    }
+}
